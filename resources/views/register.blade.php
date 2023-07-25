7f6bed9230e9033ecc79ffbe00b87f7946064afb
Raihan Lail <raihanlail07@gmail.com>
2023-07-25 11:41:58 +0700

    add register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -41,33 +41,33 @@
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-white dark:border-gray-500">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-3xl font-bold  px-32 md:px-40 items-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black justify-center">
-                            Login
+                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
+                            Register
                         </h1>
-                        <p class="font-semibold text-sm md:text-base">Selamat datang di SIMONEV Badan Pusat Statistik</p>
                         <form class="space-y-4 md:space-y-6" action="#">
                             <div>
+                                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Name</label>
+                                <input type="email" name="email" id="email" placeholder="Masukkan nama" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            </div>
+                            <div>
                                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Email</label>
-                                <input type="email" name="email" id="email" placeholder="Masukkan email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white- dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                                <input type="email" name="email" id="email" placeholder="Masukkan email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                             </div>
                             <div>
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
                                 <input type="password" name="password" id="password" placeholder="Masukkan password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                      <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                      <label for="remember" class="text-gray-500 dark:text-black">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                            <div>
+                                <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm password</label>
+                                <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             </div>
-                            <button type="submit" class="w-full text-white bg-sky-500 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <div class="flex items-start">
+                                
+                                
+                            </div>
+                            <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-500 dark:hover:bg-sky-700 dark:focus:ring-primary-800">Create an account</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="./register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                Already have an account? <a href="./login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                             </p>
                         </form>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::get('/privacy', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
